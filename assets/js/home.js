92ee89f4c332ac12788576a8638f5a1711a5bfe7
saidsn <saidsn@code.edu.az>
2022-09-10 20:00:56 +0400

    Slider

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,29 @@
 "use strict"
+$(function () {
+
+    let swiper = new Swiper(".mySwiper", {
+        loop: true,
+    })
+    let swiperSlider = new Swiper(".my-slider", {
+        loop:true,
+        slidesPerView: 4,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+
+
+
+
+   
+
+
+
+
+
+
+
+});
