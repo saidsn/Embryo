fddc59ea72000c82cdb2ab3eb88205d992b82370
saidsn <saidsn@code.edu.az>
2022-09-11 16:02:31 +0400

    Section-Sunglasses

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,16 +4,7 @@ $(function () {
     let swiper = new Swiper(".mySwiper", {
         loop: true,
     })
-    let swiperSlider = new Swiper(".my-slider", {
-        loop:true,
-        slidesPerView: 4,
-        spaceBetween: 30,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
-
+   
 
 
 
