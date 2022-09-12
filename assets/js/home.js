a683d5beafeff21d2d4a43eac466796a038fc060
saidsn <saidsn@code.edu.az>
2022-09-12 11:13:15 +0400

    scroll section add

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,17 +4,17 @@ $(function () {
     let swiper = new Swiper(".mySwiper", {
         loop: true,
     })
+   let scrollSection = document.getElementById("scroll-section");
    
 
+   window.onscroll = function() {scrollFunction()};
 
-
-
-   
-
-
-
-
-
-
-
+   function scrollFunction() {
+     if (document.body.scrollTop > 195|| document.documentElement.scrollTop > 195) {
+        scrollSection.style.top = "0";
+     } else {
+        scrollSection.style.top = "-62px";
+        $("div").removeClass("inActive");
+     }
+   }
 });
