7e3180b12b38179fee315b2fe91861a928488d52
saidsn <saidsn@code.edu.az>
2022-09-14 12:01:18 +0400

    add card section

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,6 +4,9 @@ $(function () {
     let swiper = new Swiper(".mySwiper", {
         loop: true,
     })
+
+
+    
    let scrollSection = document.getElementById("scroll-section");
    
 
@@ -17,4 +20,71 @@ $(function () {
         $("div").removeClass("inActive");
      }
    }
+
+
+  
+
+
 });
+function openTabMenu(evt, TabMenu) {
+   var i, tabcontent, tablinks;
+   tabcontent = document.getElementsByClassName("tabcontent");
+   for (i = 0; i < tabcontent.length; i++) {
+     tabcontent[i].style.display = "none";
+   }
+   tablinks = document.getElementsByClassName("tablinks");
+   for (i = 0; i < tablinks.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" active", "");
+   }
+   document.getElementById(TabMenu).style.display = "block";
+   evt.currentTarget.className += " active";
+ }
+
+
+var swiper1 = new Swiper("#tab-menu .mySwiper1", {
+   slidesPerView: 4,
+   spaceBetween: 10,
+   loop: true,
+   autoplay: {
+     delay: 4000,
+   },
+ });
+ 
+ var swiper3 = new Swiper("#tab-menu .mySwiper3", {
+   slidesPerView: 4,
+   spaceBetween: 30,
+ 
+ });
+ 
+ var swiper4 = new Swiper("#tab-menu .mySwiper4", {
+   slidesPerView: 4,
+   spaceBetween: 30,
+ });
+
+ var swiper = new Swiper(".mySwiper2", {
+   slidesPerView:5,
+   loop:true,
+   spaceBetween:20,
+   autoplay:{
+       delay:3000,
+       disableOnInteraction:false,
+   },
+   breakpoints:{
+       0:{
+           slidesPerView:1,
+       },
+       550:{
+           slidesPerView:2,
+       },
+       800:{
+           slidesPerView:4,
+       },
+       1000:{
+           slidesPerView:5,
+       },
+   },
+   pagination:{
+       el:".swiper-pagination",
+       clickable:true,
+   }
+ });
